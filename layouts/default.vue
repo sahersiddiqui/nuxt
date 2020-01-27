6ce328505e3e28116679f0398cae0e89cc9ef377
saher <saher>
2020-01-27 19:14:34 +0530

    changes for reponsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,104 +1,104 @@
 <template>
 <div>
     <header class="md-layout">
-            <div class="md-layout-item md-size-15">
-                <figure>
-                    <img src="/images/logo.png">
-                </figure>
-            </div>
-            <div class="md-layout-item ">
-                <md-menu md-size="medium" md-align-trigger>
-                    <md-button md-menu-trigger>
-                        Explore
-                        <md-icon md-src="icons/keyboard_arrow_down.svg"/> 
-                    </md-button>
-                    <md-menu-content>
-                        <md-menu-item>My Item 1</md-menu-item>
-                        <md-menu-item>My Item 2</md-menu-item>
-                        <md-menu-item>My Item 3</md-menu-item>
-                    </md-menu-content>
-                </md-menu>
-                <input type="text"/>
-            </div>
-            <div class="md-layout-item">
-            </div>
+        <div class="md-layout-item md-size-15">
+            <figure>
+                <img src="/images/logo.png">
+            </figure>
+        </div>
+        <div class="md-layout-item ">
+            <md-menu md-size="medium" md-align-trigger>
+                <md-button md-menu-trigger>
+                    Explore
+                    <md-icon md-src="icons/keyboard_arrow_down.svg" />
+                </md-button>
+                <md-menu-content>
+                    <md-menu-item>My Item 1</md-menu-item>
+                    <md-menu-item>My Item 2</md-menu-item>
+                    <md-menu-item>My Item 3</md-menu-item>
+                </md-menu-content>
+            </md-menu>
+            <input type="text" />
+        </div>
+        <div class="md-layout-item">
+        </div>
     </header>
-    <section >
-        <nuxt/>
+    <section>
+        <nuxt />
     </section>
     <footer >
-                <!--Footer Nav Option-->
-            <div class="md-layout">
-                <div class="md-layout-item">
-                    <span class="footer_title">About Us</span>
-                    <div class="footer-list">
-                        <ul>
-                            <li><a href="https://gurushala.co/page/story">Our Journey</a></li>
-                            
-                            <li><a href="https://gurushala.co/contact-us">Contact Us</a></li>
-                        </ul>
-                    </div>
+        <!--Footer Nav Option-->
+        <div class="md-layout">
+            <div class="md-layout-item">
+                <span class="footer_title">About Us</span>
+                <div class="footer-list">
+                    <ul>
+                        <li><a href="https://gurushala.co/page/story">Our Journey</a></li>
+
+                        <li><a href="https://gurushala.co/contact-us">Contact Us</a></li>
+                    </ul>
                 </div>
-                <div class="md-layout-item">
-                    <span class="footer_title">Gallery</span>
-                    <div class="footer-list">
-                        <ul>
-                            
-                            <li><a href="https://gurushala.photo.blog/" target="_blank">Photos</a>
-                            </li>
-                            <li><a href="https://www.youtube.com./channel/UCY3vP3nIEhMff0IqxWy46Gw" target="_blank">Videos</a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="md-layout-item">
+                <span class="footer_title">Gallery</span>
+                <div class="footer-list">
+                    <ul>
+
+                        <li><a href="https://gurushala.photo.blog/" target="_blank">Photos</a>
+                        </li>
+                        <li><a href="https://www.youtube.com./channel/UCY3vP3nIEhMff0IqxWy46Gw" target="_blank">Videos</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="md-layout-item">
-                    <span class="footer_title">Get Involved</span>
-                    <div class="footer-list">
-                        <ul>
-                            <li><a href="https://gurushala.co/page/volunteer">Volunteer</a></li>
-                            <li>
-                                <a href="https://gurushala.co/page/join-us">Careers</a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="md-layout-item">
+                <span class="footer_title">Get Involved</span>
+                <div class="footer-list">
+                    <ul>
+                        <li><a href="https://gurushala.co/page/volunteer">Volunteer</a></li>
+                        <li>
+                            <a href="https://gurushala.co/page/join-us">Careers</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="md-layout-item">
-                    <span class="footer_title">Partners</span>
-                    <div class="footer-list">
-                        <ul>
-                            <li>
-                                <a href="https://gurushala.co/partner/school-institue">School  Institutions</a>
-                            </li>
-                            <li>
-                                <a href="https://gurushala.co/partner/content-collaborator">Content Collaborator</a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="md-layout-item">
+                <span class="footer_title">Partners</span>
+                <div class="footer-list">
+                    <ul>
+                        <li>
+                            <a href="https://gurushala.co/partner/school-institue">School Institutions</a>
+                        </li>
+                        <li>
+                            <a href="https://gurushala.co/partner/content-collaborator">Content Collaborator</a>
+                        </li>
+                    </ul>
                 </div>
+            </div>
         </div>
         <!--Footer Nav Option Close-->
         <!--Brand Partner-->
         <div class="brand_wrapper md-layout">
-                    <div class="md-layout-item">
-                        <figure class="footer-logo">
-                            <img src="https://gurushala.co/front/images/foot-logo.png" alt="gurushala">
-                        </figure>
-                    </div>
-                    <div class="md-layout-item">
-                        <figure class="footer-logo">
-                            <img src="https://gurushala.co/front/images/pratham-logo.png" alt="pratham">
-                        </figure>
-                    </div>
-                    <div class="md-layout-item">
-                        <figure class="footer-logo">
-                            <img src="https://gurushala.co/front/images/vodafone-logo.png" alt="vodafone">
-                        </figure>
-                    </div>
-                    <div class="md-layout-item">
-                        <figure class="footer-logo">
-                            <img src="https://gurushala.co/front/images/indus-tower.png" alt="indus-tower">
-                        </figure>
-                    </div>
+            <div class="md-layout-item">
+                <figure class="footer-logo">
+                    <img src="https://gurushala.co/front/images/foot-logo.png" alt="gurushala">
+                </figure>
+            </div>
+            <div class="md-layout-item">
+                <figure class="footer-logo">
+                    <img src="https://gurushala.co/front/images/pratham-logo.png" alt="pratham">
+                </figure>
+            </div>
+            <div class="md-layout-item">
+                <figure class="footer-logo">
+                    <img src="https://gurushala.co/front/images/vodafone-logo.png" alt="vodafone">
+                </figure>
+            </div>
+            <div class="md-layout-item">
+                <figure class="footer-logo">
+                    <img src="https://gurushala.co/front/images/indus-tower.png" alt="indus-tower">
+                </figure>
+            </div>
         </div>
         <!--Brand Partner Close-->
         <!--Copy Right Footer-->
@@ -106,7 +106,7 @@
             <div class="custom_container">
                 <div class=" md-layout vertical_mid">
                     <div class="md-layout-item">
-                        <p class="cpyt">© 2020  gurushala. All rights reserved.</p>
+                        <p class="cpyt">© 2020 gurushala. All rights reserved.</p>
                     </div>
                     <div class="md-layout-item">
                         <div class="tnc_nav">
@@ -115,7 +115,7 @@
                             <li><a href="https://gurushala.co/page/faq">FAQ</a></li>
                         </div>
                     </div>
-                            
+
                 </div>
             </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,94 @@
 <template>
 <div class="md-layout">
-            <div class=" md-layout-item md-size-30">
-                    <div class="offer_option">
-                        <ul>
-                            <nuxt-link title="Leaderboard" to="/leaderboard">
-                                <li class="cr">Leaderboard</li>
-                            </nuxt-link>
-                            
-                            <a title="Teacher Communities" href="https://gurushala.co/lac/area">
-                                <li class="cd">Teacher Communities</li>
-                            </a>
-                            <a title="Teaching Modules" href="https://gurushala.co/course">
-                                <li class="ed">Teaching Modules</li>
-                            </a>
-                            <a title="Teacher Stories" href="https://gurushala.co/ed-tech?category%5B%5D=70">
-                                <li class="nt">Teacher Stories</li>
-                            </a>
-                        </ul>
-                    </div>
-            </div>
-            <div class=" md-layout-item">
-                <h2 class="section_title text-left">Gurushala Offers</h2>
-                <div class="in">
+    <div class=" md-layout-item md-size-30 md-small-size-100 md-medium-size-30 ">
+        <div class="offer_option ">
+            <ul>
+                <nuxt-link title="Leaderboard" to="/leaderboard">
+                    <li class="cr">Leaderboard</li>
+                </nuxt-link>
 
-                    <div class="md-layout">
-                        <div class="md-layout-item">
-                            <div class="offer_card">
-                                <a href="https://gurushala.co/course">
-                                    <div class="inner_wrap">
-                                        <figure class="offer_cousres_img">
-                                            <img src="/images/box1.JPG" data-src="/images/box1.JPG" class=" ls-is-cached lazyloaded">
-                                        </figure>
-                                        <span class="offer_title">Courses</span>
-                                        <p>
-                                            Enroll in self-paced, interactive and free to use online courses on innovative pedagogies, classroom management et all
-                                        </p>
-                                    </div>
-                                </a>
+                <a title="Teacher Communities" href="https://gurushala.co/lac/area">
+                    <li class="cd">Teacher Communities</li>
+                </a>
+                <a title="Teaching Modules" href="https://gurushala.co/course">
+                    <li class="ed">Teaching Modules</li>
+                </a>
+                <a title="Teacher Stories" href="https://gurushala.co/ed-tech?category%5B%5D=70">
+                    <li class="nt">Teacher Stories</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+    <div class="md-layout-item">
+        <h2 class="section_title text-left">Gurushala Offers</h2>
+        <div class="in">
+            <div class="md-layout">
+                <div class="md-layout-item md-medium-size-50 md-small-size-100">
+                    <div class="offer_card">
+                        <a href="https://gurushala.co/course">
+                            <div class="inner_wrap">
+                                <figure class="offer_cousres_img">
+                                    <img src="/images/box1.JPG" data-src="/images/box1.JPG" class=" ls-is-cached lazyloaded">
+                                </figure>
+                                <span class="offer_title">Courses</span>
+                                <p>
+                                    Enroll in self-paced, interactive and free to use online courses on innovative pedagogies, classroom management et all
+                                </p>
                             </div>
-                        </div>
-                        <div class="md-layout-item">
-                            <div class="offer_card">
-                                <a href="https://gurushala.co/content">
-                                    <div class="inner_wrap">
-                                        <figure class="offer_cousres_img">
-                                            <img src="/images/box2.JPG" data-src="/images/box2.JPG" class=" ls-is-cached lazyloaded">
-                                        </figure>
-                                        <span class="offer_title">Content Library</span>
-                                        <p>
-                                            Explore a diverse range of free to use and curriculum-aligned online content
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
+                        </a>
                     </div>
-
-                    <div class="flex-row">
-                        <div class="md-layout-item">
-                            <div class="offer_card">
-                                <a href="https://gurushala.co/ed-tech">
-                                    <div class="inner_wrap">
-                                        <figure class="offer_cousres_img">
-                                            <img src="/images/box3.JPG" data-src="/images/box3.JPG" class=" ls-is-cached lazyloaded">
-                                        </figure>
-                                        <span class="offer_title">Research Corner</span>
-                                        <p>
-                                            Update yourself with diverse research and inspiring real-life stories on themes like pedagogy, ed-tech and much more
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="md-layout-item">
-                            <div class="offer_card">
-                                <a href="https://gurushala.co/networking">
-                                    <div class="inner_wrap">
-                                        <figure class="offer_cousres_img">
-                                            <img src="/images/box4.JPG" data-src="/images/box4.JPG" class=" ls-is-cached lazyloaded">
-                                        </figure>
-                                        <span class="offer_title">Staffroom</span>
-                                        <p>
-                                            Connect and engage in discussion groups with teachers across India and share your classroom experiences
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
+                <div class="md-layout-item md-medium-size-50 md-small-size-100">
+                    <div class="offer_card">
+                        <a href="https://gurushala.co/content">
+                            <div class="inner_wrap">
+                                <figure class="offer_cousres_img">
+                                    <img src="/images/box2.JPG" data-src="/images/box2.JPG" class=" ls-is-cached lazyloaded">
+                                </figure>
+                                <span class="offer_title">Content Library</span>
+                                <p>
+                                    Explore a diverse range of free to use and curriculum-aligned online content
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
+            <div class="flex-row">
+                <div class="md-layout-item md-medium-size-50 md-small-size-100">
+                    <div class="offer_card">
+                        <a href="https://gurushala.co/ed-tech">
+                            <div class="inner_wrap">
+                                <figure class="offer_cousres_img">
+                                    <img src="/images/box3.JPG" data-src="/images/box3.JPG" class=" ls-is-cached lazyloaded">
+                                </figure>
+                                <span class="offer_title">Research Corner</span>
+                                <p>
+                                    Update yourself with diverse research and inspiring real-life stories on themes like pedagogy, ed-tech and much more
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="md-layout-item md-medium-size-50 md-small-size-100">
+                    <div class="offer_card">
+                        <a href="https://gurushala.co/networking">
+                            <div class="inner_wrap">
+                                <figure class="offer_cousres_img">
+                                    <img src="/images/box4.JPG" data-src="/images/box4.JPG" class=" ls-is-cached lazyloaded">
+                                </figure>
+                                <span class="offer_title">Staffroom</span>
+                                <p>
+                                    Connect and engage in discussion groups with teachers across India and share your classroom experiences
+                                </p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
     </div>
 </div>
