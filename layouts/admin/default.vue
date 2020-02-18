6b3b65ff734662581e540f3dbebb29e9ec5f9499
saher <najmussaher.siddiqui@appinventiv.com>
2020-02-18 15:46:25 +0530

    admin panel

--- layouts/admin/default.vue
+++ layouts/admin/default.vue
@@ -6,6 +6,12 @@
 				style="background-color: rgb(238, 241, 246)"
 			>
 				<el-menu :default-openeds="['1', '3']">
+					<el-menu-item>
+						<template slot="title">
+							<i class="el-icon-message"></i>
+							Gurushala
+						</template>
+					</el-menu-item>
 					<el-submenu index="1">
 						<template slot="title"
 							><i class="el-icon-message"></i>Navigator
